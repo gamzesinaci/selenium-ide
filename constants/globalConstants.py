47c5500c4fe78bf84d64444e8f3e525d92271311
gamze sinaci <gamze-manga.1999@hotmail.com>
2024-01-10 19:13:43 +0300

    guncelleme

---
+++ constants/globalConstants.py
@@ -0,0 +1,7 @@
+BASE_URL = "https://www.saucedemo.com"
+invalid_login_xlsx = "data/invalid.xlsx"
+USERNAME_ID = "user-name"
+PASSWORD_ID = "password"
+LOGIN_BUTTON_ID ="login-button"
+ERROR_MESSAGE_XPATH = "//*[@id='login_button_container']/div/form/div[3]/h3"
+USERNAME_PASSWORD_DONT_MATCH = "Epic sadface: Username and password do not match any user in this service"
--- test_pytest_ikii.py
+++ test_pytest_ikii.py
@@ -1,23 +1,48 @@
-from typing import Literal
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
-from selenium.webdriver.support.wait import WebDriverWait #ilgili driver bekleten yapı
-from selenium.webdriver.support import expected_conditions  as ec #beklenen koşullar (webDriver verilen koşullar)
-from selenium.webdriver.common.action_chains import ActionChains #elementlerı birden fazla işlem yapmak istiyordak kullanırız
+from selenium.webdriver.support.wait import WebDriverWait #ilgili driverı bekleten yapı
+from selenium.webdriver.support import expected_conditions as ec #beklenen koşullar
 import pytest
+import openpyxl
+from constants import globalConstants as c 
 
 
-class Test_Pytest:
-
-    def setup_method(self): #her test başlangıcında çalışacak fonksiyon
+class Test:
+    def setup_method(self): #her test başlangıcında çalışacak fonk
         self.driver = webdriver.Chrome()
         self.driver.get("https://www.saucedemo.com")
-        self.driver.maximize_window() #ekranı büyütür
-      
-    def teardown_method(self): #her seferinde testin bitinminde çalışacak fonksiyon
+        self.driver.maximize_window() 
+
+    def teardown_method(self): # her testinin bitiminde çalışacak fonk
         self.driver.quit()
+    
+    
+    def getData(): #veri çağırdığımızda self operatörünü çağırmıyoruz.
+        excel = openpyxl.load_workbook(c.invalid_login_xlsx)
+        sheet = excel["Sayfa1"] #hangi sayfada çalışacağını gösteriyoruz
+        rows = sheet.max_row #kaçıncı satıra kadar veri var ona bakılır
+        data = []
+
+        for i in range(2,rows+1):#ilki username ve password yani başlık olduğu için wxcelin ikinci satırından başladık ve row a kadar aldık
+            username = sheet.cell(i,1).value # username başlığın değerine ulaştık. ama içindeki değere de ulaşmak için value filtresi koyduk
+            password = sheet.cell(i,2).value
+            data.append((username,password))#username ve password bir data olduğu için tekrar parantez içine aldık
+            
+        return data 
+
+    @pytest.mark.parametrize("username,password",getData())
+    def test_invalid_login(self,username,password):
+        usernameInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,c.USERNAME_ID)))
+        usernameInput.send_keys(username)
+        passwordInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,c.PASSWORD_ID)))
+        passwordInput.send_keys(password)
+        loginButton = self.driver.find_element(By.ID,c.LOGIN_BUTTON_ID)
+        loginButton.click()
+        errorMessage = self.driver.find_element(By.XPATH,c.ERROR_MESSAGE_XPATH)
+        assert errorMessage.text == c.USERNAME_PASSWORD_DONT_MATCH
+        sleep(5)
 
     @pytest.mark.parametrize("username,password",[("1","secret_sauce"),("problem_user","1"),("error_user","1")])
     def test_invalid_login(self,username,password):
@@ -32,9 +57,9 @@ class Test_Pytest:
         loginButton.click()
         
         errorMessage = self.driver.find_element(By.XPATH,"//*[@id='login_button_container']/div/form/div[3]/h3")
-        assert  errorMessage.text == "Epic sadface: Username and password do not match any user in this service"
+        assert  errorMessage.text == c.USERNAME_PASSWORD_DONT_MATCH
     
-    @pytest.mark.parametrize("username,password",[("standard_user","secret_sauce")])
+    @pytest.mark.skip("username,password",[("standard_user","secret_sauce")])
     def test_valid_remove(self,username,password):
         usernameInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"user-name")))
         usernameInput.send_keys(username)
@@ -56,7 +81,7 @@ class Test_Pytest:
         assert remove.text == "Remove"
         sleep(5)
 
-    @pytest.mark.parametrize("username,password",[("standard_user","secret_sauce")])
+    @pytest.mark.skip("username,password",[("standard_user","secret_sauce")])
     def test_filter(self,username,password):
         usernameInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"user-name")))
         usernameInput.send_keys(username)
